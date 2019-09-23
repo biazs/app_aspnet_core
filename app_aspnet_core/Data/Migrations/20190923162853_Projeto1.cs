c7f0bf3789ab129a0d945e31d025f186ec3e7268
Bia <fabianazs@gmail.com>
2019-09-23 17:05:39 -0300

    First commit

---
+++ app_aspnet_core/Data/Migrations/20190923162853_Projeto1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace app_aspnet_core.Data.Migrations
+{
+    public partial class Projeto1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Despesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Valor = table.Column<double>(nullable: false),
+                    CategoriaId = table.Column<int>(nullable: false),
+                    DespesaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Despesas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Despesas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Despesas_Despesas_DespesaId",
+                        column: x => x.DespesaId,
+                        principalTable: "Despesas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Despesas_CategoriaId",
+                table: "Despesas",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Despesas_DespesaId",
+                table: "Despesas",
+                column: "DespesaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Despesas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
